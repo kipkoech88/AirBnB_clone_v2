544a57a336a5c5beca8c6043f7533baca28e7b35
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-16 05:04:41 -0700

    set up server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,8 @@
 # create and setup web servers for deployment
 apt-get update
 apt-get install nginx -y
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
-printf %s "<html>
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
@@ -15,5 +11,7 @@ printf %s "<html>
   </body>
 </html>
 " > /data/web_static/releases/test/index.html
-ln -s /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+sed -i "/^\tserver_name/ a\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current; \n\t}\n"  /etc/nginx/sites-available/default
+service nginx start
