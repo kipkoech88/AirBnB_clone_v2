a177d1c27979ab0cd62c7eb7033b2f01ebfd182c
kipkoech88 <hoseakipkoech88@gmail.com>
2024-05-30 02:29:39 -0700

    default var as an arg

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Initialize Flask app
+with two routes
+with strict_slashes=False
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Displays HBNB
+    when user navigates to the URL
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """ Takes in a text
+    variable and displays
+    c variabe """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pycool(text="is cool"):
+    """ Display Python
+    + passed in arg or
+    default text"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
